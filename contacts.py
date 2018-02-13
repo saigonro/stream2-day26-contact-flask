41ff56b9939afc07a085c89392a42ccc55bce7b5
Dan Dumitrache <vietnamezul@gmail.com>
2018-02-13 11:37:35 +0000

    add the id functionality

--- contacts.py
+++ contacts.py
@@ -4,31 +4,32 @@ from flask import render_template
 app = Flask(__name__)
 
 contacts = {
-    'Dan': {'name': 'Dan', 'email': 'vietnamezul@gmail.com', 'phone': '0899592515'},
-    'Bula': {'name': 'Bula', 'email': 'bula@gmail.com', 'phone': '0899592515'},
-    'Strula': {'name': 'Strula', 'email': 'strula@gmail.com', 'phone': '0899592515'},
+    # '1': {'id': 1, 'name': 'Dan', 'email': 'vietnamezul@gmail.com', 'phone': '0899592515'},
+    # '2': {'id': 1, 'name': 'Bula', 'email': 'bula@gmail.com', 'phone': '0899592515'},
+    # '3': {'id': 1, 'name': 'Strula', 'email': 'strula@gmail.com', 'phone': '0899592515'},
     }
-
+the_id = 0
 @app.route("/", methods=['GET', 'POST'])
+
 def hello():
+    
+    global the_id
     if request.method == "POST":
         # This is what the user types
-        name_contact = request.form.get('name_contact')
+        # name_contact = request.form.get('name_contact')
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
-        
-        
-        contacts[name] = {'name': name, 'email': email, 'phone': phone}
-        
+        contacts[the_id] = {'id': the_id, 'name': name, 'email': email, 'phone': phone}
+        the_id += 1
         # contacts.update({'name': name, 'email': email, 'phone': phone})
         # contacts.update({'name_contact': name_contact, 'value_contact': {'name': name, 'email': email, 'phone': phone}})
     return render_template('contacts.html', data=contacts.values())
     
 @app.route("/delete", methods=['POST'])
 def delete_contact():
-    name_to_delete = request.form.get('contact_to_delete')
-    del(contacts[name_to_delete])
+    id = int(request.form.get('contact_to_delete'))
+    del(contacts[id])
     return render_template('contacts.html', data=contacts.values())
     
 if __name__ == "__main__":
